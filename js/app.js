24e5eef9d05001474526f46dd46baa19a0e9922a
Adriana Becerril <Adriana@MacBook-Air-de-Arisbeth.local>
2018-02-28 21:19:15 -0600

    reto resuelto

--- js/app.js
+++ js/app.js
@@ -1,41 +1,52 @@
-const form = document.getElementById('search-form');
-const searchField = document.getElementById('search-keyword');
-const responseContainer = document.getElementById('response-container');
+//traer elementos
+const form = document.getElementById("search-form");
+const searchField = document.getElementById("search-keyword");
+const responseContainer = document.getElementById("response-container");
 let searchedForText;
 
-form.addEventListener('submit', function(e){
+//agregando evento al formulario
+form.addEventListener("submit", function(e) {
     e.preventDefault();
-    responseContainer.innerHTML = '';
+    responseContainer.innerHTML = "";
     searchedForText = searchField.value;
     getNews();
 });
 
+//funcion para crear las peticiones
 function getNews() {
-    console.log(searchedForText)
-    const articleRequest = new XMLHttpRequest();
-    articleRequest.open(`GET`, `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=990cd4440d8144d89ab304393b77ab2e`);
-    articleRequest.onload = addNews;
-    articleRequest.onerror = handleError;
-    articleRequest.send();
-
-}
+    const articleRequest = new XMLHttpRequest(); //crear objeto
+    articleRequest.open("GET", `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e98c9111b40848d9a1c05597baaf3024`); //metodo open para iniciar request
+    articleRequest.onload = addNews; // funcion ejecutada abajo en la que funciona la peticion
+    articleRequest.onerror = handleError; //funcion ejecutada abajo para mostrar en consola mensaje de error
+    articleRequest.send(); //envia peticion al servidor
+};
 
 function handleError() {
-    console.log('Se ha presentado un error');
-}
+    console.log("se ha producido un error");
+};
 
+//funcion para agregar noticias al html dependiendo del filtro de busqueda
 function addNews() {
-    const data = JSON.parse(this.responseText);
-    const article = data.response.docs[0]
-    const articleArray = data.response.docs;
-    const title = article.headline.main;
-    const snippet = article.snippet;
-articleArray.forEach(element =>{
-    let li = document.createElement('li');
-    li.className = 'articleClass';
-    li.innerText = snippet;
-    responseContainer.appendChild(li)
 
-})
+    const data = JSON.parse(this.responseText); //objeto de la data
+    //console.log(data);
 
-}
+    const response = data.response.docs; //arreglo de objetos de la propiedad docs del objeto response de la data
+    //console.log(response);
+    const arraySplice = response.splice(0, 5); // array con los objetos seleccionados de acuerdo a los indices cortados que se indican con el metodo splice
+    //console.log(arraySplice);
+
+    response.forEach((element, index, array) => {
+
+        const snippet = element.snippet; //accediendo a la propiedad snippet del arreglo
+        //console.log(snippet);
+        const multimedia1 = element.multimedia; //accediendo a los objetos multimedia del arreglo
+        //console.log(multimedia1);
+
+        let li = document.createElement("li");
+        li.className = "articleClass";
+        li.innerText = snippet;
+
+        responseContainer.appendChild(li);
+    });
+};
